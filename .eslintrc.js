816c22fdbfb80e9f888bd066cdef40e8f1ca3a15
Yang <704799639@qq.com>
2020-06-16 23:17:33 +0800

    首次提交

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+      browser: true,
+      es6: true,
+      node: true,
+  },
+
+  globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+  },
+
+  parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+      parser: 'babel-eslint',
+  },
+
+  rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+      'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+      semi: [2, 'always'],
+      quotes: [2, 'single', { allowTemplateLiterals: true }],
+  },
+
+  root: true,
+
+  extends: ['eslint:recommended'],
+};
---
+++ routes/users.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+// eslint-disable-next-line no-unused-vars
+router.get('/', function(req, res, next) {
+  res.json({name: 'yangyankang', age: 17});
+});
+
+module.exports = router;
