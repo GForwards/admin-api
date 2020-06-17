641090db9538eaf375628accafc83f4166343db1
Yang <704799639@qq.com>
2020-06-17 11:13:37 +0800

    完成eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,30 @@
 module.exports = {
-  env: {
-      browser: true,
-      es6: true,
-      node: true,
-  },
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    parser: 'babel-eslint',
 
-  globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-  },
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
 
-  parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
-      parser: 'babel-eslint',
-  },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        parser: 'babel-eslint',
+    },
 
-  rules: {
-      'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
-      'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-      semi: [2, 'always'],
-      quotes: [2, 'single', { allowTemplateLiterals: true }],
-  },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+        semi: [2, 'always'],
+        quotes: [2, 'single', { allowTemplateLiterals: true }],
+    },
 
-  root: true,
+    root: true,
 
-  extends: ['eslint:recommended'],
+    extends: ['eslint:recommended', 'prettier', 'plugin:prettier/recommended'],
 };
--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 // eslint-disable-next-line no-unused-vars
-router.get('/', function(req, res, next) {
-  res.json({name: 'yangyankang', age: 17});
+router.get('/', function (req, res, next) {
+    res.json({ name: 'yangyankang', age: 17 });
 });
 
 module.exports = router;
