7e3a4b5c069c3e072f395ec2b9eeea3a1ae819ba
Yang <704799639@qq.com>
2020-06-30 11:51:36 +0800

    修改环境变量配置文件以及修复数据库连着失败bug

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 function listen() {
     if (app.get('env') === 'test') return;
-    app.listen(port);
+    // app.listen(port);
     console.log('Express app started on port ' + port);
 }
 
